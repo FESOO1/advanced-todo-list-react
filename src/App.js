5a85ec8edd528d01a04e5bc2a48245aeea57f70b
FES001 <firdavs7794948@gmail.com>
2025-08-10 00:54:19 +0500

    Working on it.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,45 @@ export default function App() {
 };
 
 function TodoList() {
+    const [todos, setTodos] = useState(todosArr);
+    const [currentFilter, setCurrentFilter] = useState('ALL');
+    const [input, setInput] = useState('');
+    const [filterButtons, setFilterButtons] = useState([{ id: 0, text: 'ALL', isActive: true }, { id: 1, text: 'IN PROGRESS', isActive: false }, { id: 2, text: 'DONE', isActive: false }]);
 
+    // HANDLE INPUT
+    function handleInput(value) {
+        setInput(value);
+    };
+
+    // HANDLE FILTER
+    function handleFilter(i) {
+        if (filterButtons[i].text !== currentFilter) {
+            const newButtonsArr = filterButtons.slice('');
+            for (const newButton of newButtonsArr) {
+                newButton.isActive = false;
+            };
+            newButtonsArr[i].isActive = true;
+            setCurrentFilter(newButtonsArr[i].text);
+            setFilterButtons(newButtonsArr);
+        };
+    };
+
+    // HANDLE CHECKBOX
+    function handleCheckbox(i) {
+        const todosArr = todos.slice('');
+        if (!todosArr[i].isDone) {
+            todosArr[i].isDone = true;
+        } else {
+            todosArr[i].isDone = false;
+        };
+        setTodos(todosArr);
+    };
 
     return (
         <main>
             <TodoListHeader />
-            <TodoListInputFilter />
-            <TodoListOutput todos={todos} />
+            <TodoListInputFilter input={input} onChangeInput={handleInput} filterButtons={filterButtons} onClickFilterButtons={handleFilter} />
+            <TodoListOutput todos={todos} currentFilter={currentFilter} onChangeCheckbox={handleCheckbox} />
         </main>
     );
 };
@@ -52,19 +84,19 @@ function TodoListHeader() {
 
 // TODO LIST INPUT FILTER
 
-function TodoListInputFilter() {
+function TodoListInputFilter({ input, onChangeInput, filterButtons, onClickFilterButtons }) {
     return (
         <div className='main-input-filter'>
-            <TodoListInput />
-            <TodoListFilter />
+            <TodoListInput input={input} onChangeInput={onChangeInput} />
+            <TodoListFilter filterButtons={filterButtons} onClickFilterButtons={onClickFilterButtons} />
         </div>
     );
 };
 
-function TodoListInput() {
+function TodoListInput({ input, onChangeInput }) {
     return (
         <form className='main-input'>
-            <input type='text' placeholder='Add a todo fing list...' className='main-input-itself' />
+            <input type='text' placeholder='Add a todo fing list...' className='main-input-itself' value={input} onChange={(e) => onChangeInput(e.target.value)} />
             <button className='main-input-button' type='submit'>
                 <svg className='main-input-button-svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
                     <path d="M12 4V20M20 12H4" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -74,8 +106,7 @@ function TodoListInput() {
     );
 };
 
-function TodoListFilter() {
-    const filterButtons = [{ text: 'ALL', isActive: true }, { text: 'IN PROGRESS', isActive: false }, { text: 'DONE', isActive: false }];
+function TodoListFilter({ filterButtons, onClickFilterButtons }) {
 
     return (
         <div className='main-filter'>
@@ -84,51 +115,63 @@ function TodoListFilter() {
                 const classes = filterButton.isActive ? 'main-filter-button main-filter-button-active' : 'main-filter-button';
 
                 return (
-                    <button key={text} className={classes} type='button'>{text}</button>
+                    <button key={text} className={classes} type='button' onClick={() => onClickFilterButtons(filterButton.id)}>{text}</button>
                 );
             })}
         </div>
     );
 };
 
-function TodoListOutput({ todos }) {
-    console.log(todos);
+function TodoListOutput({ todos, currentFilter, onChangeCheckbox }) {
+
+
+    console.log(currentFilter);
     return (
         <div className='main-outputs'>
             {todos.map(todo => {
-                const isDone = todo.isDone;
-                const classes = isDone ? 'main-output main-output-done' : 'main-output';
-                const idText = todo.id + 1;
-                const value = todo.value.length > 45 ? todo.value.slice(0, 45) + '...' : todo.value;
 
-                return (
-                    <div key={todo.id} className={classes}>
-                        <div className='main-output-checkbox'>
-                            <input type='checkbox' className='main-output-checkbox-input' /* checked={isDone} */ />
-                        </div>
-                        <button className='main-output-edit-button' type='button'>
-                            <h5 className='main-output-edit-button-id'>#{idText}</h5>
-                            <p className='main-output-edit-button-todo-text'>{value}</p>
-                            {/* <h6 className='main-output-edit-button-id'>ADDED: {todo.date}</h6> */}
-                        </button>
-                        <button type='button' className='main-output-delete-button'>
-                            <svg className='main-output-delete-button-svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
-                                <path d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
-                                <path d="M3 5.5H21M16.0557 5.5L15.3731 4.09173C14.9196 3.15626 14.6928 2.68852 14.3017 2.39681C14.215 2.3321 14.1231 2.27454 14.027 2.2247C13.5939 2 13.0741 2 12.0345 2C10.9688 2 10.436 2 9.99568 2.23412C9.8981 2.28601 9.80498 2.3459 9.71729 2.41317C9.32164 2.7167 9.10063 3.20155 8.65861 4.17126L8.05292 5.5" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
-                                <path d="M9.5 16.5L9.5 10.5" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
-                                <path d="M14.5 16.5L14.5 10.5" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
-                            </svg>
-                        </button>
-                    </div>
-                );
+                if (currentFilter === 'IN PROGRESS' && !todo.isDone) {
+                    return <TodoListOuputItself key={todo.id} todo={todo} onChangeCheckbox={onChangeCheckbox} />
+                } else if (currentFilter === 'DONE' && todo.isDone) {
+                    return <TodoListOuputItself key={todo.id} todo={todo} onChangeCheckbox={onChangeCheckbox} />
+                } else if (currentFilter === 'ALL') {
+                    return <TodoListOuputItself key={todo.id} todo={todo} onChangeCheckbox={onChangeCheckbox} />
+                };
             })}
+        </div>
+    );
+};
+
+function TodoListOuputItself({ todo, onChangeCheckbox }) {
+    const isDone = todo.isDone;
+    const classes = isDone ? 'main-output main-output-done' : 'main-output';
+    const idText = todo.id + 1;
+    const value = todo.value.length > 45 ? todo.value.slice(0, 45) + '...' : todo.value;
+
+    return (
+        <div className={classes}>
+            <div className='main-output-checkbox'>
+                <input type='checkbox' className='main-output-checkbox-input' checked={isDone} onChange={(e) => onChangeCheckbox(todo.id)} />
+            </div>
+            <button className='main-output-edit-button' type='button'>
+                <h5 className='main-output-edit-button-id'>#{idText}</h5>
+                <p className='main-output-edit-button-todo-text'>{value}</p>
+            </button>
+            <button type='button' className='main-output-delete-button'>
+                <svg className='main-output-delete-button-svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+                    <path d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
+                    <path d="M3 5.5H21M16.0557 5.5L15.3731 4.09173C14.9196 3.15626 14.6928 2.68852 14.3017 2.39681C14.215 2.3321 14.1231 2.27454 14.027 2.2247C13.5939 2 13.0741 2 12.0345 2C10.9688 2 10.436 2 9.99568 2.23412C9.8981 2.28601 9.80498 2.3459 9.71729 2.41317C9.32164 2.7167 9.10063 3.20155 8.65861 4.17126L8.05292 5.5" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
+                    <path d="M9.5 16.5L9.5 10.5" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
+                    <path d="M14.5 16.5L14.5 10.5" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
+                </svg>
+            </button>
         </div>
     );
 };
 
 
 // 
-const todos = [
+const todosArr = [
     {
         id: 0,
         isDone: false,
