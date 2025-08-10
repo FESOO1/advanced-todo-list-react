9ef0fcb85b3d65ad36b3c6b94a001f50db33dbdc
FES001 <firdavs7794948@gmail.com>
2025-08-10 21:46:36 +0500

    more

--- src/App.js
+++ src/App.js
@@ -267,6 +267,13 @@ function TodoListOutputEmpty() {
                 <path d="M8 8V10" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
                 <path d="M8 15V17" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path>
             </svg>
+            <svg className='main-output-empty-svg-second' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+                <ellipse cx="12" cy="5" rx="8" ry="3" stroke="black" strokeWidth="1.5"></ellipse>
+                <path d="M20 12C20 13.6569 16.4183 15 12 15C7.58172 15 4 13.6569 4 12" stroke="black" strokeWidth="1.5"></path>
+                <path d="M20 5V19C20 20.6569 16.4183 22 12 22C7.58172 22 4 20.6569 4 19V5" stroke="black" strokeWidth="1.5"></path>
+                <path d="M8 8V10" stroke="black" strokeWidth="1.5" strokeLinecap="round"></path>
+                <path d="M8 15V17" stroke="black" strokeWidth="1.5" strokeLinecap="round"></path>
+            </svg>
         </div>
     );
 };
